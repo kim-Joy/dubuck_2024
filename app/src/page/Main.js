3b996db30c845756ce0f094b6e98a1ca3e6ad62b
kim-Joy <85498186+kim-Joy@users.noreply.github.com>
2024-02-14 18:27:45 +0900

    슬슬 퇴근하자..

--- app/src/page/Main.js
+++ app/src/page/Main.js
@@ -24,7 +24,7 @@ function Main() {
             </button>
           </Link>
         </div>
-        <ul className="list-group category inner20">
+        <ul className="list-group category">
           {calendar_item.map((item) =>(
             <li className={`item ${item.className}` }>
               <Link to={item.to}>
