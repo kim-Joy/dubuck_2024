23a23abad14175f9807c48d2ad3c15ed4ba685c5
kim joy <didlshakdi@gmail.com>
2024-06-30 18:08:16 +0900

    .

--- app/src/page/Result_View.js
+++ app/src/page/Result_View.js
@@ -10,8 +10,8 @@ function Result_View() {
   const today_list = [
     {key: "1", answer: 'exist', verb: '존재하다', listen: "", my_answer: "", my_verb: '살아있다' , wrong: 'wrong'},
     {key: "2", answer: 'extreme', verb: '극단', listen: "", my_answer: "extremes", my_verb: '', wrong: 'wrong'  },
-    {key: "3", answer: 'original', verb: '원래의', listen: "", my_answer: "ORIGINAL", my_verb: '존재하다' ,wrong: '' },
-    {key: "4", answer: 'soon a after', verb: '곧이어', listen: "", my_answer: "exists", my_verb: '곧이어', wrong: '' }
+    {key: "3", answer: 'original', verb: '원래의', listen: "", my_answer: "ORIGINAL", my_verb: '' ,wrong: '' },
+    {key: "4", answer: 'soon a after', verb: '곧이어', listen: "", my_answer: "", my_verb: '곧이어', wrong: '' }
 
   ]
 
